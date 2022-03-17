1cb8b7bb29b140321a701ac5b56cf0f49c1b61f6
TaRiQ-Ali-4 <targali498@gmail.com>
2022-03-17 10:21:46 +0200

    add my project

---
+++ app/src/main/java/com/targ498/tharwtak/MainActivity.java
@@ -0,0 +1,14 @@
+package com.targ498.tharwtak;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
